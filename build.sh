c7627ea84e5e3313fc012d3d2f7caa72361d5bcb
Mohamed Saad Ibn Seddik <ms.ibnseddik@gmail.com>
2023-08-02 22:56:00 -0400

    adds build scripts

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p build
+
+pushd build
+
+# if macOS, add qt5 to path
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    export PATH="$(brew --prefix qt5)/bin":$PATH
+fi
+
+# prepare build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+
+# build plugins
+cmake --build . --target ArduPilotPlugin -- -j$(nproc)
+cmake --build . --target ParachutePlugin -- -j$(nproc)
+
+# build stream_camera
+cmake --build . --target stream_camera -- -j$(nproc)
+
+popd
